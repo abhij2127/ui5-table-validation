9a1df03e3eb1b754c1f7987dc01142b7a8d034f5
Abhishek Joshi <55267403+abhij2127@users.noreply.github.com>
2026-03-22 09:24:16 +0000

    Minor commit

--- src/ui5/odataTabValidation/ODataTabValidation/utils/Validation.js
+++ src/ui5/odataTabValidation/ODataTabValidation/utils/Validation.js
@@ -1,7 +1,14 @@
 sap.ui.define(
     ["sap/ui/core/Messaging"],
     (Validator) => {
-
+        /**
+         * @typedefinition ValidationResult
+         * @type {object}
+         * @property {string} type - The type of the validation result (e.g., "Error", "Warning", "Information").
+         * @property {string} message - The validation message describing the issue.
+         * @property {string} path - The binding path to the relevant data in the model.
+         * @property {object} context - The context object containing additional information about the validation result.
+         */
         const MESSAGE_TYPE = {
             ERROR: "Error",
             WARNING: "Warning",
@@ -28,6 +35,8 @@ sap.ui.define(
                 this._waitServiceMetadataLoaded().then(()=>{
                     const oMetdata = this.oDataModel.getServiceMetadata();
                 })
+                const aContexts = oTable.getBinding().getContexts();
+                const aResults = [];
             },
             /**
              * @description Waits for the OData service metadata to be loaded before performing any operations that depend on it.
@@ -45,7 +54,17 @@ sap.ui.define(
                         });
                     }
                 });
-            }
+            },
+            /**
+             * 
+             * @param {sap.ui.model.Context} oContext 
+             * @returns {ValidationResult[]} An array of validation results for the given context.
+             * @private
+             */
+            _validateContexts(oContext){
+                
+            },
+
 
         }
 
